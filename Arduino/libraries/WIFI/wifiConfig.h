e077109d39e9e568528960453aa7594b797697f2
YukiMOB <yuki.rx0126@gmail.com>
2018-06-14 18:14:38 +0900

    second

--- Arduino/libraries/WIFI/wifiConfig.h
+++ Arduino/libraries/WIFI/wifiConfig.h
@@ -1,7 +1,7 @@
 #ifndef WIFICONFIG_H
 #define WIFICONFIG_H
 
-static const char *kWifiSsid = "xxxx";
-static const char *kWifiPass = "yyyyyy";
+static const char *kWifiSsid = "mogmog";
+static const char *kWifiPass = "068gundam";
 
 #endif
